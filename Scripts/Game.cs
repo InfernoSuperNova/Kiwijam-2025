5e0b15aa0eaa56b8ad69dca1d50e19cba957bd48
Karl Bennett <widemouthfrog1@gmail.com>
2025-07-26 20:06:23 +1200

    Hook up activations and points

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -9,11 +9,19 @@ public partial class Game : Node3D
     public List<StaticBody3D> itemSlots = [];
     Camera3D camera;
     StaticBody3D selectedSlot;
+    RichTextLabel activationText;
+    long activations = 100;
+    RichTextLabel pointsText;
+    long points = 10;
 
     public override void _Ready()
     {
         base._Ready();
         Node3D activeItemSlots = GetNode<Node3D>("ItemSlots/Active");
+        activationText = GetNode<RichTextLabel>("Control/ColorRect/Activations");
+        activationText.Text = "Activations Left: " + activations;
+        pointsText = GetNode<RichTextLabel>("Control/ColorRect/Points");
+        pointsText.Text = "Points: " + points;
         foreach (Node node in activeItemSlots.GetChildren())
         {
             if(node is StaticBody3D itemSlot)
@@ -26,12 +34,48 @@ public partial class Game : Node3D
 
     }
 
+    public void ActivateButton()
+    {
+        activations--;
+        activationText.Text = "Activations Left: " + activations;
+        points++;
+        pointsText.Text = "Points: " + points;
+    }
+
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
         if(@event is InputEventMouseMotion mouseMotionEvent)
         {
             OnMouseMove(mouseMotionEvent);
+        }
+
+        if (@event is InputEventMouse mouseEvent )
+        {
+            OnMousePressed(mouseEvent);
+        }
+    }
+
+    public void OnMousePressed(InputEventMouse mouseEvent)
+    {
+        Vector3 start = camera.ProjectRayOrigin(mouseEvent.Position);
+        Vector3 end = camera.ProjectPosition(mouseEvent.Position, 10);
+
+        PhysicsRayQueryParameters3D parameters = new();
+        parameters.From = start;
+        parameters.To = end;
+        Godot.Collections.Dictionary result = GetWorld3D().DirectSpaceState.IntersectRay(parameters);
+        if (result.Count > 0)
+        {
+            StaticBody3D body = (StaticBody3D)result["collider"];
+            if (body.GetChildren().Count < 5)
+            {
+                selectedSlot = body;
+            }
+            else
+            {
+                selectedSlot = null;
+            }
         }
     }
 
@@ -59,24 +103,24 @@ public partial class Game : Node3D
 
     public override void _Process(double deltaTime)
     {
-        foreach(StaticBody3D body in itemSlots)
+        if(isBuying)
         {
-            Node3D node = (Node3D)body.FindChild("Item_Slot");
-            
-            node.Show();
-            node = (Node3D)body.FindChild("Item_Slot_Red");
-            node.Hide();
-            GD.Print(selectedSlot);
-
-            if (selectedSlot != null)
+            foreach (StaticBody3D body in itemSlots)
             {
-                node = (Node3D)selectedSlot.FindChild("Item_Slot");
-                
-                node.Hide();
-                node = (Node3D)selectedSlot.FindChild("Item_Slot_Red");
-                GD.Print(node);
+                Node3D node = (Node3D)body.FindChild("Item_Slot");
                 node.Show();
+                node = (Node3D)body.FindChild("Item_Slot_Red");
+                node.Hide();
+
+                if (selectedSlot != null)
+                {
+                    node = (Node3D)selectedSlot.FindChild("Item_Slot");
+                    node.Hide();
+                    node = (Node3D)selectedSlot.FindChild("Item_Slot_Red");
+                    node.Show();
+                }
             }
         }
+        
     }
 }
