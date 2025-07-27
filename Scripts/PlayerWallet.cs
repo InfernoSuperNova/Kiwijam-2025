c7434f3c6f47eda994d800bad3062a9350f225b6
Gxaps <williamtetzlaff123@gmail.com>
2025-07-27 14:09:48 +1200

    Auto stash before merge of "main" and "origin/main"

---
+++ Scripts/PlayerWallet.cs
@@ -0,0 +1,15 @@
+namespace Kiwijam2025;
+
+public class PlayerWallet
+{
+    public static float sp = 0;
+    public static float mp = 0;
+    public static float lp = 0;
+
+    public static void ResetPoints()
+    {
+        sp = 0;
+        mp = 0;
+        lp = 0;
+    }
+}
